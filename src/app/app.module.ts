b40971177fbf372fad568e5ce127e3c0dff05ce4
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-08-28 16:54:25 +0530

    first attempt success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
