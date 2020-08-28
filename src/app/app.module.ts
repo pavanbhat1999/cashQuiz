14ffcf9d0438f827967663fd46269f23a5b11cc5
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-08-28 16:44:51 +0530

    corrected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LogInComponent } from './components/log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LogInComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
