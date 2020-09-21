c3e975f0c897b5ad43f48cbf985ea6be3777b8c2
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-09-21 12:59:58 +0530

    red heart addition is done 1 O clock

--- src/app/deposite-page/deposite-page.component.ts
+++ src/app/deposite-page/deposite-page.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deposite-page.component.css']
 })
 export class DepositePageComponent implements OnInit {
-
+count_r : number =1;
+count_y : number = 2;
   constructor() { }
 
   ngOnInit(): void {
